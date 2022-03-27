2082ee9a070e2058cac1348044b2101a5164f97a
ilepekhov <lepvanek@gmail.com>
2022-03-27 23:09:03 +0500

    Added the initial solution structure and the example of the Quick Sort algorithm

---
+++ QuickSort/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] QuickSort(int[] array)
+{
+    if (array.Length < 2)
+        return array;
+
+    var pivot = array[0];
+
+    var less = array[1..].Where(x => x <= pivot).ToArray();
+    var greater = array[1..].Where(x => x > pivot).ToArray();
+
+    return QuickSort(less).Concat(new[] {pivot}).Concat(QuickSort(greater)).ToArray();
+}
+
+foreach (var i in QuickSort(new []{10, 5, 2, 2, 3}))
+{
+    Console.Write(i + " ");
+}
